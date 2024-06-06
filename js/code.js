ab27b2bc7c694567bfc47714e0ad5a0cf9960636
nurali-code <nur3.dav.97@gmail.com>
2024-06-06 17:38:25 +0500

    main mob done

--- js/code.js
+++
@@ -1,1 +0,0 @@
-const qrCode = "REFGS72H";
--- js/script.js
+++ js/script.js
@@ -1,40 +1,24 @@
-const qrBtn = document.getElementById('qrBtn');
-const qrAmount = document.getElementById('amount');
+$('.btn__menu, .nav__link').on('click', function () {
+    if ($(window).innerWidth() <= 1000) {
+        $('.nav, .btn__menu, body').toggleClass('is__active');
+    }
+})
 
-document.getElementById('qrCodeText').innerText = qrCode;
-
-qrBtn.onclick = (e) => {
-    e.target.disabled = true;
-    navigator.clipboard.writeText(qrCode)
-        .then(function () {
-            setTimeout(() => { e.target.disabled = false; }, 3000);
-            updateCopyCount();
-        })
-        .catch(function (err) {
-            console.error('Failed to copy text:', err);
-            e.target.disabled = false;
-        });
-};
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    fetch('get_copy_count.php')
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) { qrAmount.innerText = data.copyCount; }
-            else { console.error('Failed to load copy count:', data.message); }
-        }).catch(error => { console.error('Error:', error); });
+$('.competitions-slider').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: false,
+    mobileFirst: true,
+    variableWidth: true,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: "unslick",
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
 });
-
-function updateCopyCount() {
-    fetch('update_copy_count.php', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ action: 'copy' })
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) { qrAmount.innerText = data.copyCount; }
-            else { console.error('Failed to update copy count:', data.message); }
-        })
-        .catch(error => { console.error('Error:', error); });
-}
