6616c6b53240071f84c2b0425fa610d66a31c9ff
nurali-code <nur3.dav.97@gmail.com>
2024-06-07 00:06:01 +0500

    all done

--- js/script.js
+++ js/script.js
@@ -1,24 +1,29 @@
 $('.btn__menu, .nav__link').on('click', function () {
-    if ($(window).innerWidth() <= 1000) {
-        $('.nav, .btn__menu, body').toggleClass('is__active');
-    }
+  if ($(window).innerWidth() <= 1000) {
+    $('.nav, .btn__menu, body').toggleClass('is__active');
+  }
+})
+$('.lang__btn').on('click', function () {
+  // $('.lang-content').toggleClass('is__active');
+  $(this).parent().toggleClass('is__active');
+
 })
 
 $('.competitions-slider').slick({
-    dots: false,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 1,
-    centerMode: false,
-    mobileFirst: true,
-    variableWidth: true,
-    responsive: [
-        {
-          breakpoint: 1024,
-          settings: "unslick",
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: false,
+  mobileFirst: true,
+  variableWidth: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: "unslick",
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
